80c25813b6ece5809285e528ad52491e35b7844d
Philipsco <bagus09051998@gmail.com>
2022-08-30 16:51:23 +0700

    meanmbahkan route untuk edit dan delete

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,36 @@
-import {addUsersHandler, showAllUserHandler, showDetailUserHandler,showUserByDateHandler} from './handlers.js'
+import {addUsersHandler, showAllUserHandler, showDetailUserHandler,showUserByDateHandler,editUserByIdHandler,deleteUserByIdHandler} from './handlers.js'
 
 const routes = [
     {
-        method: 'POST',
-        path: '/users',
-        handler: addUsersHandler,
+        method : 'POST',
+        path : '/users',
+        handler : addUsersHandler,
     },
-
     {
-        method: 'GET',
-        path: '/users',
-        handler: showAllUserHandler,
+        method : 'GET',
+        path : '/users',
+        handler : showAllUserHandler,
     },
-
     {
-        method: 'GET',
-        path: `/users/{userName}`,
-        handler: showDetailUserHandler,
+        method : 'GET',
+        path : `/users/{userName}`,
+        handler : showDetailUserHandler,
     },
     {
         method : 'POST',
         path : '/users/date',
         handler : showUserByDateHandler,
     },
+    {
+        method : 'PUT',
+        path : '/users/{userId}',
+        handler : editUserByIdHandler
+    },
+    {
+        method : 'DELETE',
+        path : '/users/{userId}',
+        handler : deleteUserByIdHandler
+    }
 ]
 
 export default routes
